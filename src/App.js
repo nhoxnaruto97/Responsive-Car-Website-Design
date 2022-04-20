1156505bc10c8c8f13b255ed6500133f988ef64d
NguyenCD <dinhnguyen.k15@gmail.com>
2022-04-20 15:32:00 +0700

    [Implement] - Implement Home & About components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./css/base.css";
 import "./css/media.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <main className="main">
+        <Home></Home>
+        <About></About>
+      </main>
     </div>
   );
 }
