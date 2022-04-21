dd2ca535f7f5b32f58e47672a43200b2cddaad1f
NguyenCD <dinhnguyen.k15@gmail.com>
2022-04-21 09:06:55 +0700

    [Fix] - Fix Home & About component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,48 +1,49 @@
 import React from "react";
 import "../css/home.css";
+import pic from "../img/home.png";
 
 const Home = () => {
   return (
-    <section class="home section" id="home">
-      <div class="shape shape__big"></div>
-      <div class="shape shape__small"></div>
+    <section className="home section" id="home">
+      <div className="shape shape__big"></div>
+      <div className="shape shape__small"></div>
 
-      <div class="home__container container grid">
-        <div class="home__data">
-          <h1 class="home__title">Choose The Best Car</h1>
-          <h2 class="home__subtitle">Porsche Mission E</h2>
-          <h3 class="home__elec">
-            <i class="ri-flashlight-fill"></i> Electric car
+      <div className="home__container container grid">
+        <div className="home__data">
+          <h1 className="home__title">Choose The Best Car</h1>
+          <h2 className="home__subtitle">Porsche Mission E</h2>
+          <h3 className="home__elec">
+            <i className="ri-flashlight-fill"></i> Electric car
           </h3>
         </div>
 
-        <img src="assets/img/home.png" alt="" class="home__img" />
-        <div class="home__car">
-          <div class="home__car--data">
-            <div class="home__car--icon">
-              <i class="ri-temp-cold-line"></i>
+        <img src={pic} alt="" className="home__img" />
+        <div className="home__car">
+          <div className="home__car--data">
+            <div className="home__car--icon">
+              <i className="ri-temp-cold-line"></i>
             </div>
-            <h2 class="home__car--nummber">24°</h2>
-            <h3 class="home__car--name">TEMPERATURE</h3>
+            <h2 className="home__car--nummber">24°</h2>
+            <h3 className="home__car--name">TEMPERATURE</h3>
           </div>
 
-          <div class="home__car--data">
-            <div class="home__car--icon">
-              <i class="ri-dashboard-3-line"></i>
+          <div className="home__car--data">
+            <div className="home__car--icon">
+              <i className="ri-dashboard-3-line"></i>
             </div>
-            <h2 class="home__car--nummber">873</h2>
-            <h3 class="home__car--name">MILEAGE</h3>
+            <h2 className="home__car--nummber">873</h2>
+            <h3 className="home__car--name">MILEAGE</h3>
           </div>
 
-          <div class="home__car--data">
-            <div class="home__car--icon">
-              <i class="ri-flashlight-fill"></i>
+          <div className="home__car--data">
+            <div className="home__car--icon">
+              <i className="ri-flashlight-fill"></i>
             </div>
-            <h2 class="home__car--nummber">94%</h2>
-            <h3 class="home__car--name">BATTERY</h3>
+            <h2 className="home__car--nummber">94%</h2>
+            <h3 className="home__car--name">BATTERY</h3>
           </div>
         </div>
-        <a href="#section" class="home__button">
+        <a href="#section" className="home__button">
           START
         </a>
       </div>
